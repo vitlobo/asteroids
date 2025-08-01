08936fc650e14413b29e0a6e8a46d90e2a76cc64
vitlobo <awhite1196@proton.me>
2025-08-01 00:29:03 -0400

    CH3-L1: Limit FPS, add player sprite and movement.

--- main.py
+++ main.py
@@ -1,21 +1,32 @@
 import pygame
 from constants import *
+from player import Player
 
 def main():
+    pygame.init()
+
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
 
-    pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    frame_clock = pygame.time.Clock()
+    dt = 0
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
+    player = Player(x = SCREEN_WIDTH / 2, y = SCREEN_HEIGHT / 2)
 
-    should_exit = False
-    while should_exit == False:
-          for event in pygame.event.get():
+    while True:
+        for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-          screen.fill((0,0,0))
-          pygame.display.flip()
+        screen.fill("black")
+        updatable.update(dt)
+        for d in drawable:
+            d.draw(screen)
+        pygame.display.flip()
+        dt = frame_clock.tick(60) / 1000
 
 def fill(self, color):
     pygame.Surface.fill(color)
